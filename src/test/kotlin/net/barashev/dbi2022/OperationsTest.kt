463a9740360e50a7c9437e5459260302dc21811d
Dmitry Barashev <dmitry@barashev.net>
2022-09-29 16:03:46 +0300

    Fake nested loops implementation and test

--- src/test/kotlin/net/barashev/dbi2022/OperationsTest.kt
+++ src/test/kotlin/net/barashev/dbi2022/OperationsTest.kt
@@ -1,7 +1,10 @@
 package net.barashev.dbi2022
 
+import net.datafaker.Faker
 import org.junit.jupiter.api.Test
+import java.util.*
 import java.util.function.Function
+import kotlin.random.Random
 import kotlin.test.assertEquals
 
 class FakeMergeSort(private val accessMethodManager: AccessMethodManager, private val cache: PageCache): MultiwayMergeSort {
@@ -18,6 +21,35 @@ class FakeMergeSort(private val accessMethodManager: AccessMethodManager, privat
             }
         }
         return "output"
+    }
+
+}
+
+class FakeNestedLoops(private val accessMethodManager: AccessMethodManager, private val pageCache: PageCache) : InnerJoin {
+    override fun <T : Comparable<T>> join(leftTable: JoinOperand<T>, rightTable: JoinOperand<T>): JoinOutput {
+        val output = mutableListOf<Pair<ByteArray, ByteArray>>()
+        accessMethodManager.createFullScan(leftTable.tableName) { leftBytes ->
+            leftTable.joinAttribute.apply(leftBytes) to leftBytes
+        }.forEach {leftTuple ->
+            accessMethodManager.createFullScan(rightTable.tableName) { rightBytes ->
+                rightTable.joinAttribute.apply(rightBytes) to rightBytes
+            }.forEach { rightTuple ->
+                if (leftTuple.first == rightTuple.first) {
+                    output.add(leftTuple.second to rightTuple.second)
+                }
+            }
+        }
+        return object : JoinOutput {
+            private val iterator = output.iterator()
+            override fun hasNext(): Boolean = iterator.hasNext()
+
+            override fun next(): Pair<ByteArray, ByteArray> = iterator.next()
+
+            override fun close() {
+            }
+
+        }
+
     }
 
 }
@@ -40,5 +72,47 @@ class OperationsTest  {
             intField().first.fromBytes(it).first
         }.toList())
     }
+
+    @Test
+    fun `fake nested loops`() {
+        Operations.innerJoinFactory = { accessMethodManager, pageCache, joinAlgorithm ->
+            when (joinAlgorithm) {
+                JoinAlgorithm.NESTED_LOOPS -> FakeNestedLoops(accessMethodManager, pageCache)
+                else -> TODO("Join algorithm $joinAlgorithm not implemented yet")
+            }
+        }
+        val storage = createHardDriveEmulatorStorage()
+        val cache = SimplePageCacheImpl(storage, 20)
+        val accessMethodManager = SimpleAccessMethodManager(cache)
+        val fooOid = accessMethodManager.createTable("foo")
+
+        val faker = Faker()
+        cache.getAndPin(accessMethodManager.addPage(fooOid)).use {fooPage ->
+            (1..10).forEach { fooPage.putRecord(Record2(intField(it), stringField(faker.name().fullName())).asBytes()) }
+        }
+
+        val barOid = accessMethodManager.createTable("bar")
+        cache.getAndPin(accessMethodManager.addPage(barOid)).use {barPage ->
+            (1..20).forEach { barPage.putRecord(Record2(
+                intField(Random.nextInt(10)),
+                dateField(Date.from(faker.date().birthday().toInstant()))).asBytes()
+            ) }
+        }
+
+        Operations.innerJoinFactory(accessMethodManager, cache, JoinAlgorithm.NESTED_LOOPS).join(
+            JoinOperand("foo") { Record2(intField(), stringField()).fromBytes(it).value1 },
+            JoinOperand("bar") { Record2(intField(), dateField()).fromBytes(it).value1 }
+        ).use {
+            while (it.hasNext()) {
+                val matchingTuples = it.next()
+                val leftRecord = Record2(intField(), stringField()).fromBytes(matchingTuples.first)
+                val rightRecord = Record2(intField(), dateField()).fromBytes(matchingTuples.second)
+                println(leftRecord)
+                println(rightRecord)
+                println("--------")
+            }
+        }
+    }
+
 
 }
